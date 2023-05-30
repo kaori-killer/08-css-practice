6e15cadf030bd247a328acdec29a29f719e87e0a
kaori-killer <sksbthwjd@daum.net>
2023-05-30 19:14:25 +0900

    styled components

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -10,14 +10,24 @@ const Paragraph = styled.p`
 `;
 
 const BigParagraph = styled(Paragraph)`
-  font-size: 2em;
+  font-size: 1em;
+`;
+
+function HelloWorld({className}: React.HTMLAttributes<HTMLElement>) {
+	return (
+		<BigParagraph className={className}>
+            Hello, World
+			<strong>!</strong>
+		</BigParagraph>
+	);
+}
+
+const SmallHelloWorld = styled(HelloWorld)`
+    font-size: 0.5em;
 `;
 
 export default function Greeting() {
 	return (
-		<BigParagraph>
-            Hello, World
-			<strong>!</strong>
-		</BigParagraph>
+		<SmallHelloWorld />
 	);
 }
